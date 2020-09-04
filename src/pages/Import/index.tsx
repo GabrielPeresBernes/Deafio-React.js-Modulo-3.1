fbfe96ff84bd2bb67245684a5c254ece3436adc9
Gabriel Peres Bernes <gabriel.p_b@hotmail.com>
2020-09-03 21:25:29 -0300

    challenge done

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(async uploadedFile => {
+      data.append('file', uploadedFile.file);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+      try {
+        await api.post('/transactions/import', data);
+      } catch (err) {
+        console.log(err.response.error);
+      }
+    });
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const fileProps = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(fileProps);
   }
 
   return (
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,7 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number | string): string =>
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(typeof value === 'string' ? parseFloat(value) : value);
 
 export default formatValue;
